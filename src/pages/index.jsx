ada4ef94d8b45b018dae962a8ccc24aa9c0c8bed
zardron <zardron.pesquera1993@gmail.com>
2024-04-18 15:16:54 +0400

    add some minor changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,13 +12,13 @@ const index = () => {
             <img src={LOGO} alt="" className="w-16 lg:w-24 z-30" />
             <div>
               <p
-                className="relative name-logo z-10 uppercase text-2xl lg:text-4xl font-bold 
+                className="relative name-logo z-10 uppercase text-xl lg:text-4xl font-bold 
                   bg-gradient-to-r from-purple-500 via-[#33bfdf] to-blue-500 
                   text-transparent bg-clip-text animate-gradient [text-shadow:_2px_3px_0_rgb(0_0_0_/20%)]"
               >
                 Zardron Pesquera
               </p>
-              <p className="relative z-10 text-[12px] lg:text-sm font-bold tracking-[.34em] pb-1 lg:p-0 lg:tracking-[.59em] text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 uppercase">
+              <p className="relative z-10 text-[10px] lg:text-sm font-bold tracking-[.34em] pb-1 lg:p-0 lg:tracking-[.59em] text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 uppercase">
                 I turn coffee into code.
               </p>
             </div>
